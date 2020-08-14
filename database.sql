c448307d6231c22596fcaa0aa5f1bda50835ed6a
Nick King <nrking0@gmail.com>
2020-08-14 12:51:45 -0500

    Database additions

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE pernmentors;
+
+CREATE TABLE mentors(
+    mentor_id SERIAL PRIMARY KEY,
+    fname VARCHAR(100),
+    lname VARCHAR(100),
+    email VARCHAR(100),
+    number VARCHAR(20),
+    grade VARCHAR(10),
+     bio VARCHAR(500),
+     interestedSubjects VARCHAR(1000)
+);
